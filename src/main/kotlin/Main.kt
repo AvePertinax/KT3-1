b7761f3061d67505831d3a0cebc63393ea99dc9c
Vlad Gromov <vladoffrevan@mail.ru>
2023-05-29 22:58:37 +0300

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,13 +6,13 @@ import posts.Post
 import posts.PostType
 
 fun main() {
-    WallService.add(Post(ownerId = 1, text = "12345"))
+    WallService.add(Post(ownerId = 1, text = "14120"))
     //Thread.sleep(1000)
-    WallService.add(Post(ownerId = 1, text = "67890"))
-    WallService.add(Post(ownerId = 2, text = "200000"))
-    WallService.add(Post(ownerId = 2, text = "300000", postType = PostType.REPLY))
+    WallService.add(Post(ownerId = 1, text = "132249"))
+    WallService.add(Post(ownerId = 2, text = "141303"))
+    WallService.add(Post(ownerId = 2, text = "402010", postType = PostType.REPLY))
 
-    WallService.add(Post(ownerId = 3, text = "800000", likes = Likes(canLike = true)))
+    WallService.add(Post(ownerId = 3, text = "222222", likes = Likes(canLike = true)))
 
     WallService.update(Post(id = 2, text = "replaced", comments = Comments(canClose = true)))
     WallService.update(Post(id = 1, text = "new text", comments = Comments(canOpen = true), canPin = false))
